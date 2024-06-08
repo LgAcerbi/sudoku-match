e450cfc7cd0e9846ced0115d097d46fb036f7d0d
lgacerbi <lgacerbi@gmail.com>
2024-06-08 17:39:52 -0300

    feat(sudoku-api): user handler

---
+++ sudoku-api/src/handlers/user-handler.go
@@ -0,0 +1,35 @@
+package user_handler
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+// RegisterPayload
+type RegisterPayload struct {
+	Id            int    `json:"id"`
+	Email         string `json:"email"`
+	GoogleIdToken string `json:"googleIdToken"`
+}
+
+func registerUser(c *fiber.Ctx) error {
+	registerPayload := new(RegisterPayload)
+
+	if parsePayloadErr := c.BodyParser(registerPayload); parsePayloadErr != nil {
+		log.Println(parsePayloadErr)
+		c.Status(400).JSON(&fiber.Map{
+			"message": parsePayloadErr,
+		})
+		return nil
+	}
+
+	log.Println(registerPayload)
+
+	return c.JSON(registerPayload)
+}
+
+func SetupRoutes(app *fiber.App) {
+	// Routes
+	app.Post("/register", registerUser)
+}
